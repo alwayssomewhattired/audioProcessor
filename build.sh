6a32354057968cf539b0318fa81def4bdde8462a
root <root@ip-172-31-13-249.us-east-2.compute.internal>
2025-06-01 05:35:37 +0000

    testing

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ mkdir -p "$BUILD_DIR"
 # Run cmake configure and generate build system
 cmake -B "$BUILD_DIR" -S . \
 -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" \
--DVCPKG_TARGET_TRIPLET=x64-linux
-
+-DVCPKG_TARGET_TRIPLET=x64-linux \
+-DCMAKE_PREFIX_PATH="/vcpkg/installed/x64-linux/share"
 
 # Build
 cmake --build "$BUILD_DIR"
